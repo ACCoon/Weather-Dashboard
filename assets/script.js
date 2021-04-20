06e5c1ebd1325508bed762e3b8341808a220233f
Andrew Coon <thenran@gmail.com>
2021-04-19 22:58:30 -0400

    Added functionality for forecast API call, displays 5-day weather forecast based on user search.

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,14 @@ const apiKey = '2db3a9423ac69ac536e99fac81ac089f';
 var searchHist = JSON.parse(localStorage.getItem("searchHist")) || [];
 
 function renderSearchHist() {
-
-}
-
-function renderForecast() {
-
+  
 }
 
 function handleSearchBtn(event) {
   event.preventDefault();
   let search = searchBox.value.trim();
   var apiURLBase = "https://api.openweathermap.org/data/2.5/";
+  var apiIconBase = "http://openweathermap.org/img/w/";
 
   // Check if search was empty. If so, do nothing.
   if (search.length === 0){
@@ -43,12 +40,31 @@ function handleSearchBtn(event) {
         let wind = data.wind.speed;
         let icon = data.weather[0].icon;
 
-        today.children[0].children[0].innerHTML = `${search} <img src="http://openweathermap.org/img/w/${icon}.png">`;
+        today.children[0].children[0].innerHTML = `${search} <img src=${apiIconBase}${icon}.png>`;
         today.children[0].children[1].textContent = `Temperature: ${temp}℉`;
         today.children[0].children[2].textContent = `Humidity: ${humid}%`
         today.children[0].children[3].textContent = `Wind Speed: ${wind} MPH`
       })
       .catch(err => console.log(err));
+
+      // Call to API for forecast weather data
+      fetch(`${apiURLBase}forecast?q=${search}&appid=${apiKey}&units=imperial`)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          for(let i = 0; i < 5; i++){
+            let icon = data.list[i].weather[0].icon;
+            let weather = data.list[i].weather[0].main;
+            let temp = data.list[i].main.temp;
+            let humid = data.list[i].main.humidity;
+
+            forecast.children[i].children[0].children[0].innerHTML = `${weather} <img src=${apiIconBase}${icon}.png>`;
+            forecast.children[i].children[0].children[1].textContent = `Temp: ${temp}℉`;
+            forecast.children[i].children[0].children[2].textContent = `Humidity: ${humid}%`;
+          }
+        })
+        .catch(err => console.log(err));
   }
 
 }
